dcef5a64ecb6f7f84c861d91479f733088d79ba5
mgapp <mgapp@MIKEGYO>
2025-06-03 18:12:06 -0400

    Add project files.

---
+++ TypeConversion/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("What is your age: ");
+
+string? ageText = Console.ReadLine();
+
+bool isValidInt = int.TryParse(ageText, out int age);
+
+Console.WriteLine($"This is valid: {isValidInt}. The number was {age}.");
+
+Console.Write(age + 15);
